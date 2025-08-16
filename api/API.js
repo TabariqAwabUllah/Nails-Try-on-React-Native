7bef1a2ab41ab4d3f516a48123171075a5e65408
Tabariq Awab Ullah <Tabariqawabullah@gmail.com>
2025-08-16 17:18:09 +0500

    API if want to add pic from URL

--- api/API.js
+++ api/API.js
@@ -2,20 +2,44 @@ import axios from "axios";
 import RNFS from "react-native-fs";
 import { NAIL_SEG_API_KEY } from "@env"; 
 
-
-export async function imageAPI(image) {
-    console.log("imageAPI ");
+// for camera image
+export async function imageAPI(imageGot) {
+    console.log("imageAPI ", imageGot);
     
-    const base64Image = await RNFS.readFile(image, 'base64');
+    const base64Image = await RNFS.readFile(imageGot, 'base64'); // Uncomment this line if you want to read the image from the device camera
     try {
         const response = await axios({
         method: "POST",
         url: "https://serverless.roboflow.com/seg_nail_test/1",
-        params: { api_key: NAIL_SEG_API_KEY },
-        data: base64Image,
+        params: { api_key: NAIL_SEG_API_KEY},
+        data: base64Image, // use if image is from device camera
         headers: { "Content-Type": "application/x-www-form-urlencoded" }
         
         });
+        console.log("imageAPI 2");
+
+        return response.data;
+    } catch (error) {
+        console.log("Error in imageAPI:", error);
+        
+    }
+}
+
+
+// for designed/url image
+export async function imageDesignAPI(imageGot) {
+    console.log("imageDesignAPI ", imageGot);
+    try {
+        const response = await axios({
+        method: "POST",
+        url: "https://serverless.roboflow.com/seg_nail_test/1",
+        params: { api_key: NAIL_SEG_API_KEY,
+            image: imageGot, // use if image is from URL
+        },
+        headers: { "Content-Type": "application/x-www-form-urlencoded" }
+        
+        });
+        console.log("imageDesignAPI 2");
 
         return response.data;
     } catch (error) {
